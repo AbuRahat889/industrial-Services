329f426a1a854613418b2e8a607e0baec7d23798
AbuRahat889 <aburahatshaum889@gmail.com>
2024-04-17 01:24:29 +0600

    add update info

--- src/Components/Update Profile/UpdatePro.jsx
+++ src/Components/Update Profile/UpdatePro.jsx
@@ -2,18 +2,25 @@ import { useContext, useState } from "react";
 import { AuthContex } from "../../Contex/AuthProvider";
 
 const UpdatePro = () => {
-  const { user, updateUserInfo } = useContext(AuthContex);
-  const [update, setUpdate] = useState();
-  // const {name, image} = updateUserInfo;
-  const handleUpdate = () =>{
-    setUpdate(updateUserInfo);
-  }
+    const { user, updateUserInfo } = useContext(AuthContex);
+    const {userName, image} = updateUserInfo;
+    const [isEditing, SetIsEditing] = useState(false);
+    const [editName, setEditName] = useState(userName);
+    const [editURl, setURL] = useState(image);
+
+    //handle changes
+    const handleChanges = (e)=>{
+        e.preventDefault();
+        SetIsEditing(isEditing);
+    }
+
+ 
   return (
     <div>
       <h1 className="text-5xl text-center bg-green-400 py-3">
         Update Your Profile.
       </h1>
-
+    <h1>hello . {editName} </h1>
       <div className=" text-center m-3">
       <img src={
                 user.photoURL || "https://i.ibb.co/wyYzNJz/man.jpg"
@@ -23,31 +30,53 @@ const UpdatePro = () => {
       <div className="hero ">
         <div className="hero-content lg:flex-row-reverse">
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form className="card-body" onSubmit={handleChanges}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
-                <input
+                {
+                    isEditing ? (
+                        <input
+                  value={userName}
                   type="name"
                   placeholder="name"
                   className="input input-bordered"
                   required
+                  onChange={e=>{
+                    setEditName(e.target.value);
+                  }}
                 />
+                    ): (<span>{editName}</span>)
+                }
+                
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Photo URL</span>
                 </label>
-                <input
-                  type="photo"
-                  placeholder="url"
-                  className="input input-bordered"
-                  required
+
+                {
+                    isEditing ? (
+                        <input
+                        value={image}
+                        type="text"
+                        placeholder="url"
+                        className="input input-bordered"
+                        required
+                        onChange={e=>{
+                        setURL(e.target.value);
+                  }}
                 />
+                    ): (<span>{editURl}</span>)
+                }
+
               </div>
+
               <div className="form-control mt-6">
-                <button onClick={handleUpdate} className="btn btn-primary">Save Changes</button>
+                <button className="btn btn-primary" type="submit">
+                    {isEditing ? "Save Changes" : "Edit Info"}
+                </button>
               </div>
             </form>
           </div>
